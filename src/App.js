2fc77a0ed69aad06f34c1b707d0e640eb5c5ec1e
minhduc <minh.duc.3701@gmail.com>
2019-10-15 14:53:34 +0700

    Test Custom Link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+
+const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+  return (
+    <Route
+      children={({ match }) => {
+        let active = match ? "active abc" : "";
+        return (
+          <li className={active}>
+            <Link to={to} className="my-link">
+              {label}
+            </Link>
+          </li>
+        );
+      }}
+      path={to}
+      exact={activeOnlyWhenExact}
+    ></Route>
+  );
+};
 
 class App extends React.Component {
   render() {
@@ -11,25 +30,26 @@ class App extends React.Component {
       <Router>
         <div className="App">
           {/* Menu */}
-          <nav className="navbar navbar-inverse">
+          <nav className="navbar navbar-defautl">
             <ul className="nav navbar-nav">
-              <li>
-                <NavLink activeClassName="active" exact to="/">
-                  Trang Chủ
-                </NavLink>
-              </li>
-              <li>
-                <NavLink activeClassName="active" to="/about">
-                  Giới Thiệu
-                </NavLink>
-              </li>
-              <li>
-                <NavLink activeClassName="active" to="/contact">
-                  Liên Hệ
-                </NavLink>
-              </li>
+              <MenuLink
+                to="/"
+                label="Trang Chủ"
+                activeOnlyWhenExact={true}
+              ></MenuLink>
+              <MenuLink
+                to="/about"
+                label="Giới Thiệu"
+                activeOnlyWhenExact={false}
+              ></MenuLink>
+              <MenuLink
+                to="/contact"
+                label="Liên Hệ"
+                activeOnlyWhenExact={false}
+              ></MenuLink>
             </ul>
           </nav>
+
           {/* Nội dung */}
           <Route path="/" exact component={Home}></Route>
           <Route path="/about" component={About}></Route>
