4a7525d1007a959fc50747131317de9d46105413
minhduc <minh.duc.3701@gmail.com>
2019-10-15 10:57:27 +0700

    Add Link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 class App extends React.Component {
   render() {
@@ -12,17 +13,21 @@ class App extends React.Component {
           {/* Menu */}
           <nav className="navbar navbar-inverse">
             <ul className="nav navbar-nav">
-              <li className="active">
-                <a>Trang Chủ</a>
+              <li>
+                <Link to="/">Trang Chủ</Link>
               </li>
               <li>
-                <a>Liên Hệ</a>
+                <Link to="/about">Giới Thiệu</Link>
+              </li>
+              <li>
+                <Link to="/contact">Liên Hệ</Link>
               </li>
             </ul>
           </nav>
           {/* Nội dung */}
           <Route path="/" exact component={Home}></Route>
           <Route path="/about" component={About}></Route>
+          <Route path="/contact" component={Contact}></Route>
         </div>
       </Router>
     );
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Contact extends React.Component {
+  render() {
+    return <div>Đây là trang Contact</div>;
+  }
+}
+
+export default Contact;
